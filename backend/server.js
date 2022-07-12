133072da968a9ef1f9d63d5947fd368485062395
James Enoch <jamesenoch@Jamess-MBP.attlocal.net>
2022-07-11 21:01:34 -0700

    fix env vars

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT;
+const app = express();
+
+app.get('/', (req, res) => {
+  debugger;
+  res.json({ message: 'Welcome to the Support Center' });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

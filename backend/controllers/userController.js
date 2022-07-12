cd0e18576c3b90e2201ecbd51dd99252ac2725dc
James Enoch <jamesenoch@Jamess-MBP.attlocal.net>
2022-07-11 23:09:47 -0700

    outline basic user validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,20 @@
-const registerUser = (req, res) => {
-  res.send('Register User');
-};
+const asyncHandler = require('express-async-handler');
 
-const loginUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error('All Fields Must Be Valid!');
+  }
+
+  console.log(req.body);
+  res.send('Register User');
+});
+
+const loginUser = asyncHandler(async (req, res) => {
   res.send('Login User');
-};
+});
 
 module.exports = {
   registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT;
+
 const app = express();
+app.use;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the Support Center' });
 });
 
 app.use('/api/users', require('./routes/userRoutes'));
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

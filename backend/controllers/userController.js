1b4edd5aeaf78d22fe24aa6b64787dee5cf4b731
James Enoch <jamesenoch@Jamess-MBP.attlocal.net>
2022-07-14 21:29:29 -0700

    establish db cxn. add user to db

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,44 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModels');
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+  const { name, email, password } = req.body;
 
   if (!email || !password) {
     res.status(400);
-    throw new Error('All Fields Must Be Valid!');
+    throw new Error('All fields must be valid!');
   }
 
-  console.log(req.body);
-  res.send('Register User');
+  // find if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User by that email already exists!');
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data!');
+  }
 });
 
 const loginUser = asyncHandler(async (req, res) => {

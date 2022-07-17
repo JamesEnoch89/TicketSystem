e9a6fb6d73d785b9652afad5f32b09f5338c2748
James Enoch <jamesenoch89@gmail.com>
2022-07-17 14:16:35 -0700

    expound on login and home pages

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -15,7 +15,7 @@ function Header(props) {
           </Link>
         </li>
         <li>
-          <Link to='/login'>
+          <Link to='/register'>
             <FaUser /> Register
           </Link>
         </li>

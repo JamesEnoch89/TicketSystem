6a13feddf63ae78de3c58d9a4d47d2b6eb1a4188
James Enoch <jamesenoch89@gmail.com>
2022-07-20 22:21:54 -0700

    retrieve user from local storage. set user in initial state

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,9 +1,12 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import authService from './authService';
 import { toast } from 'react-toastify';
+import { json } from 'express';
+
+const user = JSON.parse(localStorage.getItem('user'));
 
 const initialState = {
-  user: null,
+  user: user ? user : null,
   isError: false,
   isSuccess: false,
   isLoading: false,

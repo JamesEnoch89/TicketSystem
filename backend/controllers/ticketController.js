023bea72754613391dfacfe99ada4a9cec302b74
James Enoch <jamesenoch89@gmail.com>
2022-07-23 22:45:31 -0700

    create the create ticket functionality

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -4,14 +4,41 @@ const User = require('../models/userModel');
 const Ticket = require('../models/ticketModel');
 
 const getTickets = asyncHandler(async (req, res) => {
-  // const user = {
-  //   user: req.user._id,
-  // };
-  res.status(200).json({ message: 'get tickets success' });
+  // get user via id in the jwt
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found!');
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+  res.status(200).json(tickets);
 });
 
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'create ticket success' });
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error('Invalid product or description');
+  }
+
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found!');
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new',
+  });
+
+  res.status(201).json(ticket);
 });
 
 module.exports = {

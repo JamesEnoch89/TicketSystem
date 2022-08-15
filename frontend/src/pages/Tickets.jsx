04106547b1c810017b3d917dd2b002fa2cfb4bfc
James Enoch <jamesenoch89@gmail.com>
2022-08-14 20:52:59 -0700

    create tickets page and route

---
+++ frontend/src/pages/Tickets.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router';
+// import { FaUser } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+import { useSelector, useDispatch } from 'react-redux';
+import { getTickets, reset } from '../features/tickets/ticketSlice';
+import Spinner from '../components/Spinner';
+import BackButton from '../components/BackButton';
+
+function Tickets() {
+  const { tickets, isLoading, isSuccess } = useSelector(
+    (state) => state.tickets
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      if (isSuccess) {
+        dispatch(reset());
+      }
+    };
+  }, [dispatch, isSuccess]);
+
+  useEffect(() => {
+    dispatch(getTickets());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return <div>Tickets</div>;
+}
+
+export default Tickets;
